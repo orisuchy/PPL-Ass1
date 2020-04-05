c000d0d53adcb8ca35d604f78cd2206995ce7388
Unknown <suchy@post.bgu.ac.il>
2020-04-05 22:13:10 +0300

    05/04/20

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -2,22 +2,26 @@ import R, { Pred } from "ramda";
 //////////////////////////////////////////Guy said in the PS she think we need to declare type for each ver
 /* Question 1 */
 //Need to change the 'any' to generic type <T>
-export const partition = (pred: Pred, arr:any[]) => {
+export const partition = <T>(pred: Pred, arr:T[]) => {
     let arr1 = arr.filter(pred,arr);
     let arr2 = R.without(arr1,arr);
-    let arr3: Array<any>[] = [arr1,arr2];
+    let arr3: Array<T>[] = [arr1,arr2];
     return arr3;
 }
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log("partition test:")
 console.log(partition(x => x % 2 === 0, numbers));
-//hello - ori
+
 /* Question 2 */
-export const mapMat = (f:any, matrix:any[][])=>{
+//No way without "any"!!!!
+export const mapMat = <T>(f:(x:T)=>any, matrix:T[][])=>{
     return matrix.map(x=>x.map(f));
 }
+
 ///test///
-const f = (x:any) => x+1;
+console.log("\nmapMat test:")
+const f = (x:number) => x+1;
 console.log(R.compose(f));
 const mat = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ]
 console.log(mapMat((x: number) => x * x, mat));
@@ -30,6 +34,7 @@ export const composeMany : any = (arr:any[]) => {
 }
 
 ///test///
+console.log("\ncomposeMany test:")
 const squareAndHalf = composeMany([(x: number) => x / 2, (x: number) => x * x]);
 console.log(squareAndHalf(5)); // => 12.5
 const add3 = composeMany([(x: number) => x + 1, (x: number) => x + 1, (x: number) => x + 1]);
@@ -69,6 +74,7 @@ export const maxSpeed = (pokedex: Pokemon[])=>{
 let pokemon1 = {id:1, name: {english:"a",japanese:"a",chinese:"b",french:"a"},type:["Fire","b"],base:{HP:1,Attack:1,Defense:1,"Sp. Attack": 1,"Sp. Defense":1, Speed:1}}
 let pokemon2 = {id:1, name: {english:"abz",japanese:"a",chinese:"b",french:"a"},type:["a","Grass"],base:{HP:1,Attack:1,Defense:1,"Sp. Attack": 1,"Sp. Defense":1, Speed:2}}
 let pikachu = {id:1, name: {english:"abpikapika",japanese:"a",chinese:"b",french:"a"},type:["sexy","funny","Grass"],base:{HP:1,Attack:1,Defense:1,"Sp. Attack": 1,"Sp. Defense":1, Speed:3}}
+console.log("\nmaxSpeed test:")
 console.log(maxSpeed([pokemon1, pokemon2, pikachu]));
 
 
@@ -79,6 +85,7 @@ export const grassTypes = (pokedex: Pokemon[])=>{
     return names.sort();
 }
 ///test///
+console.log("\ngrassTypes test:")
 console.log(grassTypes([pokemon1, pokemon2, pikachu]));
 
 
@@ -86,4 +93,5 @@ export const uniqueTypes = (pokedex: Pokemon[])=>{
     return R.chain(x => x.type, pokedex);
 }
 ///test///
+console.log("\nuniqueTypes test:")
 console.log(uniqueTypes([pokemon1, pokemon2, pikachu]));
