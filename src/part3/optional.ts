b74bad5616c31a7d069bb18b5b3ec233692ce4d9
Unknown <suchy@post.bgu.ac.il>
2020-04-10 11:06:23 +0300

    Question3 - 1

--- src/part3/optional.ts
+++ src/part3/optional.ts
@@ -1,12 +1,21 @@
 /* Question 1 */
-//heeelooooooo
-export type Optional<T> = undefined;
+export type Optional<T> = Some<T> | None;
+    interface Some <T>{
+        tag: 'Some';
+        value: T;
+    }
 
-export const makeSome = undefined;
-export const makeNone = undefined;
+    interface None {
+        tag: 'None';
+    }
 
-export const isSome = undefined;
-export const isNone = undefined;
+
+export const makeSome = <T>(value: T): Some<T> => ({ tag: "Some", value: value});
+export const makeNone = (() => ({ tag: "None"}));
+
+export const isSome = <T>(x: Optional<T>): x is Some<T> => x.tag === "Some";
+export const isNone = <T>(x: Optional<T>): x is None => x.tag === "None";
 
 /* Question 2 */
 export const bind = undefined;
+
